9243ec74f2239eeac95fc6c9f8959daa34a8a907
mohmed mahmoued <mohmedmohaued9@gmail.com>
2026-05-31 21:56:14 +0300

    up

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 const RESTAURANT_SCHEDULE = {
-  'Bazooka':            { open: '00:00', close: '00:00' },
-  'Karam El Sham':      { open: '00:00', close: '00:00' },
-  'B Laban':            { open: '00:00', close: '00:00' },
-  'Wahmy Burger':       { open: '00:00', close: '00:00' },
-  'Koshary Al Khedewy': { open: '00:00', close: '00:00' },
-  'Hawawshi Al Refaie': { open: '00:00', close: '00:00' },
-  'Saadaawy Burger':    { open: '00:00', close: '00:00' },
-  'Krebs':              { open: '00:00', close: '00:00' },
-  'Sliceno':            { open: '00:00', close: '00:00' },
-  'Fan w Tarab':        { open: '00:00', close: '00:00' },
+  'Bazooka':            { open: '10:00', close: '3:00' },
+  'Karam El Sham':      { open: '10:00', close: '3:00' },
+  'B Laban':            { open: '10:00', close: '3:00' },
+  'Wahmy Burger':       { open: '10:00', close: '3:00' },
+  'Koshary Al Khedewy': { open: '10:00', close: '3:00' },
+  'Hawawshi Al Refaie': { open: '10:00', close: '3:00' },
+  'Saadaawy Burger':    { open: '10:00', close: '3:00' },
+  'Krebs':              { open: '10:00', close: '3:00' },
+  'Sliceno':            { open: '10:00', close: '3:00' },
+  'Fan w Tarab':        { open: '10:00', close: '3:00' },
 };
 
 function isOpen(brandKey) {
